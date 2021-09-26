226a2112a8aa6d2cfe154d9d2ab26875de7e87db
nazmul-rion <nazmulrion16@gmail.com>
2021-09-26 21:10:36 +0600

    minor fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header text-center rounded-bottom py-3 mb-3">
             <h3>Make a Team</h3>
             <h4>to participate</h4>
-            <h1>ICPC Challenge Marathon</h1>
+            <h1><b>ICPC Challenge Marathon</b></h1>
             <h4><i className="fas fa-award text-danger"></i> 1st Price: <b>$50000 </b></h4>
         </div >
     )
